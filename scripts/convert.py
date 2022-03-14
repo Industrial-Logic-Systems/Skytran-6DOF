48d46d0d6f0658f344bfb4107486bf1706d3b624
Luke Eltiste <luke343279@gmail.com>
2022-03-14 08:55:53 -0700

    change convert.py to be more robust

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,10 +1,15 @@
 import csv
 import tkinter as tk
 from tkinter import filedialog
-from unicodedata import numeric
 
 import numpy as np
 from stewart_controller import Stewart_Platform
+
+min = 0
+max = 200
+platform_height = 570
+servo_length = 650
+platform = Stewart_Platform(507, 264, platform_height, 0.1226, 0.2268)
 
 
 def verify_lengths(lengths, min, max):
@@ -12,6 +17,35 @@ def verify_lengths(lengths, min, max):
         if length < min or length > max:
             return False
     return True
+
+
+def get_converted(lines, shift_amts):
+    converted = [["Line", "Time (ms)", "X", "Y", "Z", "U", "V", "W"]]
+
+    prev_time = 0.0
+    for i, line in enumerate(lines):
+        # print(f"Calculating line: {i}")
+        cur_time = line[0] * 1000
+        convert = [i, int(cur_time - prev_time)]
+        prev_time = cur_time
+        x = line[1] * 1000 + shift_amts[0]
+        y = line[2] * 1000 + shift_amts[1]
+        z = line[3] * 1000 + shift_amts[2]
+        u = line[4]
+        v = line[5]
+        w = line[6]
+        actuator_lengths = platform.calculate(np.array([x, y, z]), np.array([u, v, w]))
+        actuator_lengths = actuator_lengths - servo_length
+        result = verify_lengths(actuator_lengths, min, max)
+        if not result:
+            print("Invalid lengths: ", actuator_lengths)
+            print("If values are less than {min} try moving the whole motion profile up by adding to the Z axis")
+            print(f"If values are greater than {max} it likely means your moving out of range of the platform")
+            return None
+        actuator_lengths = [int(x) for x in actuator_lengths]
+        convert.extend(actuator_lengths)
+        converted.append(convert)
+    return converted
 
 
 def main():
@@ -27,52 +61,47 @@ def main():
     lines = lines[1:]
     lines = [
         [
-            int(line[0]),
-            int(line[1]),
+            float(line[0]),
+            float(line[1]),
             float(line[2]),
             float(line[3]),
             float(line[4]),
             float(line[5]),
             float(line[6]),
-            float(line[7]),
         ]
         for line in lines
     ]
     # print(lines)
 
-    min = 0
-    max = 200
-    platform_height = 570
-    servo_length = 650
-    platform = Stewart_Platform(507, 264, platform_height, 0.1226, 0.2268)
+    print("Starting conversion...")
 
-    converted = [["Line", "Time (ms)", "X", "Y", "Z", "U", "V", "W"]]
+    converted = None
+    shift_amts = [0, 0, 0]
+    while converted is None:
+        converted = get_converted(lines, shift_amts)
+        if converted is None:
+            user_input = input("Would you like to shift the motion profile by any amount? (y/n)")
+            if user_input and user_input.lower()[0] == "y":
+                axis = input("Which axis would you like to shift? (x, y, z): ")
+                axis = axis.lower()
+                if axis[0] == "x":
+                    shift_amts[0] = int(input("How much would you like to shift the motion profile by? (In mm): "))
+                elif axis[0] == "y":
+                    shift_amts[1] = int(input("How much would you like to shift the motion profile by? (In mm): "))
+                elif axis[0] == "z":
+                    shift_amts[2] = int(input("How much would you like to shift the motion profile by? (In mm): "))
+                else:
+                    print("Invalid axis")
+            else:
+                return
+            print(f"Shifting X-axis by {shift_amts[0]}, Y-axis by {shift_amts[1]}, Z-axis by {shift_amts[2]}")
 
-    for line in lines:
-        print(f"Calculating line: {line[0]}")
-        convert = [line[0], line[1]]
-        x = line[2]
-        y = line[3]
-        z = line[4]
-        u = line[5]
-        v = line[6]
-        w = line[7]
-        actuator_lengths = platform.calculate(np.array([x, y, z]), np.array([u, v, w]))
-        actuator_lengths = actuator_lengths - servo_length
-        result = verify_lengths(actuator_lengths, min, max)
-        if not result:
-            print("Invalid lengths: ", actuator_lengths)
-            print("If values are less than {min} try moving the whole motion profile up by adding to the Z axis")
-            print(f"If values are greater than {max} it likely means your moving out of range of the platform")
-            return
-        actuator_lengths = [int(x) for x in actuator_lengths]
-        convert.extend(actuator_lengths)
-        converted.append(convert)
+    print("Conversion finished...")
 
-    file = filedialog.asksaveasfile(filetypes=[("CSV Files", "*.csv")], defaultextension=[("CSV Files", "*.csv")])
-    writer = csv.writer(file)
-    writer.writerows(converted)
-    file.close()
+    file = filedialog.asksaveasfilename(filetypes=[("CSV Files", "*.csv")], defaultextension=[("CSV Files", "*.csv")])
+    with open(file, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(converted)
 
 
 if __name__ == "__main__":
